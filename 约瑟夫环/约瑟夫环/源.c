09c2a786d1278ee6845e81be60c7c73f20eeddaa
杜辰龙 <2664484273@qq.com>
2019-09-25 19:43:05 +0800

    9.25 约瑟夫环 修改

---
+++ 约瑟夫环/约瑟夫环/源.c
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
+	int data;
+	int position;
+	struct node* next;
+}node;
+
+node* CreatList(node* l,int n)
+{
+	
+	int i = 0,num=0;
+	node* ret, *s;
+	ret = (node*)malloc(sizeof(node));
+	ret = l;
+	ret->next = NULL;
+	for (i = 0; i < n; i++)
+	{
+		printf("请输入第%d个数---->",i+1);
+		scanf("%d", &num);
+		s = (node*)malloc(sizeof(node));
+		s->position = i + 1;
+		s->data = num;
+		ret->next = s;
+		ret = s;
+	}
+	l = l->next;
+	ret->next = l;
+	return l;
+}
+node* DelList(node* l,node* point)
+{
+	node* p;
+	p = (node*)malloc(sizeof(node));
+	p = l;
+	l = l->next;
+	point->next = l;
+	free(p);
+	return p;
+}
+
+void game(node* l,int n)
+{
+	printf("m=");
+	int m = 0;
+	scanf("%d", &m);
+	int count = 0;
+	node* point;
+	point = (node*)malloc(sizeof(node));
+	while (l->next!=l)
+	{
+		if (count < m-1)
+		{
+			count++;
+			point = l;
+			l = l->next;
+		}
+		else
+		{
+			printf("%d ", l->position);
+			count = 0;
+			m = l->data;
+			/*node* p;
+			p = (node*)malloc(sizeof(node));
+			p = l;
+			l = l->next;
+			point->next = l;
+			free(p);*/
+			l = DelList(l, point);
+		}
+	}
+	printf("%d ", l->position);
+}
+int main()
+{
+	printf("n=");
+	int n = 0;
+	scanf("%d", &n);
+	node* l;
+	l = (node*)malloc(sizeof(node));
+	l = CreatList(l,n);
+	game(l,n);
+	/*int i = 0;
+	for (i = 0; i < 8; i++)
+	{
+		printf("%d ", l->data);
+		l = l->next;
+	}*/
+	return 0;
+}
